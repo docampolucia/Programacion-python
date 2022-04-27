814ad60d789a2eb41da44521f758b64fea1ce467
docampolucia <docampolucia@hotmail.com>
2022-04-27 20:43:08 -0300

    agregue ejercicios

---
+++ clase24-04/ejercicio1.py
@@ -0,0 +1,18 @@
+#Desarrollar un software que permita ingresar números. 
+#Por cada ingreso se debe mostrar el mayor, menor, el total y el promedio hasta el momento.
+
+try:
+	numeros = str(input("Ingrese los numeros separados por espacios: "))
+	num = list()
+	numeros.split(" ")
+	numero.add(numeros)
+except ValueError:
+	print("No ha ingresado un numero.")
+
+numero = numero.split(" ")
+print(max.numero)
+	
+
+
+
+
---
+++ clase24-04/ejercicio2.py
@@ -0,0 +1,15 @@
+#Escribir un código que muestre en orden alfabético las palabras ingresadas.
+
+palabras = []
+referencia = 1
+
+while referencia =! 0:
+    try:
+        palabra = input("Ingrese una palabra: ")
+        palabras.add = palabra
+    except TypeError:
+        print("No ha ingresado una palabra")
+
+
+
+ 
